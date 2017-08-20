5116bd836cf3906c63a0b4a55627ef0216143d78
zj-jj <532735617@qq.com>
2017-08-20 02:10:09 -0700

    求两个数的最大值

--- hello.c
+++ hello.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 
+int max(int x, int y)
+{
+	return x > y ? x : y;
+}	
 
 int main()
 {
 	int a = 3;
+	int b = 5;
+	int c = max(a, b);
 
 	printf("hello mornning.\n");
-
+	
+	printf("max is %d.\n", c);
 	
 	return 0;
 }	
